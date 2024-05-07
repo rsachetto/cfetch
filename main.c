553d07f1cb2ff201f3211379aa7e954a117dfc2e
Rafael Sachetto <rsachetto@gmail.com>
2024-05-07 12:10:18 -0300

    First commit

--- main.c
+++ main.c
@@ -1,29 +1,7 @@
-
 #include "cfetch.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
-#include <ctype.h>
-
-char *products[1024];
-double quant[1024];
-char *unit[1024];
-double value[1024];
-
-char * trim(char * s) {
-
-    if (s == NULL) return NULL;
-
-    int l = strlen(s);
-
-    if (l == 0) return NULL;
-
-    while(isspace(s[l - 1])) --l;
-    while(*s && isspace(*s)) ++s, --l;
-
-    return strndup(s, l);
-}
 
 int main(int argc, char **argv) {
 
@@ -36,7 +14,7 @@ int main(int argc, char **argv) {
         return 1;
 
     FILE *f = fopen(outfile, "wb");
-    
+
     fwrite(buffer, 1, len, f);
     fclose(f);
 
